42484706eec0874b72366a3f50134f7ad5f29cbd
Nursultan Gaiypov <gaiypov.nursultan@gmail.com>
2025-11-02 20:06:19 +0600

    fix (markers): markers display fix

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,30 @@
 import { LiteAPI } from "liteapi-map-sdk";
 import "liteapi-map-sdk/dist/liteapi-map-sdk.css";
 
-LiteAPI.Map.init({
+const map1 = LiteAPI.Map.init({
   placeId: "ChIJYeZuBI9YwokRjMDs_IEyCwo",
   proxyDomain: "lite-api-proxy.vercel.app",
-  selector: "#app",
+  selector: "#app1",
 });
 
-LiteAPI.Map.init({
+const map2 = LiteAPI.Map.init({
   placeId: "ChIJW-T2Wt7Gt4kRKl2I1CJFUsI",
   proxyDomain: "lite-api-proxy.vercel.app",
   selector: "#app2",
 });
+
+{
+  let heatmapEnabled = true;
+  document.querySelector("#toggleHeatmap1")?.addEventListener("click", () => {
+    heatmapEnabled = !heatmapEnabled;
+    map1.toggleMapHeatmap(heatmapEnabled);
+  });
+}
+
+{
+  let heatmapEnabled = true;
+  document.querySelector("#toggleHeatmap2")?.addEventListener("click", () => {
+    heatmapEnabled = !heatmapEnabled;
+    map2.toggleMapHeatmap(heatmapEnabled);
+  });
+}
